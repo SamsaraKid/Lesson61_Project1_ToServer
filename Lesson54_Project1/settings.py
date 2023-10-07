f87112a444fb652bbcf699f45164769b444c56e4
koany <koroliev@yandex.ru>
2023-10-07 23:25:10 +0300

    переименовал базу

--- Lesson54_Project1/settings.py
+++ Lesson54_Project1/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Lesson54_Project1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'dbKino.sqlite3',
+        'NAME': BASE_DIR / 'dbKinoKP.sqlite3',
     }
 }
 
