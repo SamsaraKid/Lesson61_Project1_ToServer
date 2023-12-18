9b5a2c73fac8364ff8a58da0a189be14c1198196
koany <koroliev@yandex.ru>
2023-12-18 21:03:44 +0300

    отправка письма сброса пароля

--- Lesson54_Project1/settings.py
+++ Lesson54_Project1/settings.py
@@ -130,4 +130,21 @@ LOGIN_REDIRECT_URL = 'lk'
 LOGOUT_REDIRECT_URL = 'home'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_HOST_USER = 'koanyur@gmail.com'
+# EMAIL_HOST_PASSWORD = 'smon akrs ymon gucn'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'koroliev@yandex.ru'
+DEFAULT_FROM_EMAIL = 'koroliev@yandex.ru'
+EMAIL_HOST_PASSWORD = 'fraumtpygueobnwu'
+
